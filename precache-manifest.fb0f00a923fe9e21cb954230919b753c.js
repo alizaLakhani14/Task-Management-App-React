162c0c2078510c83828b2ee14a80b866f1517420
aliza <lakhani.aliza14@gmail.com>
2020-10-06 15:51:02 +0500

    Updates

---
+++ precache-manifest.fb0f00a923fe9e21cb954230919b753c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94425790b4717b3a66ff01ed1aca3e47",
+    "url": "/task-management-app/index.html"
+  },
+  {
+    "revision": "6bf263f8cac2d2755f70",
+    "url": "/task-management-app/static/css/2.7f669fb7.chunk.css"
+  },
+  {
+    "revision": "ddb0c483596571181359",
+    "url": "/task-management-app/static/css/main.35f652c0.chunk.css"
+  },
+  {
+    "revision": "6bf263f8cac2d2755f70",
+    "url": "/task-management-app/static/js/2.2fa180a5.chunk.js"
+  },
+  {
+    "revision": "ddb0c483596571181359",
+    "url": "/task-management-app/static/js/main.d4a39c5b.chunk.js"
+  },
+  {
+    "revision": "e2f2ea9ceabc0d603d2d",
+    "url": "/task-management-app/static/js/runtime-main.10bcaca6.js"
+  }
+]);
